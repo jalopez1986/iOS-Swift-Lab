a6821aed1a948a2c660cf9f30ad13f5415b70289
Jorge Lopez <jlobeto@gmail.com>
2021-03-27 21:02:24 -0500

    Storyboard reference perform segue with identifier.

--- CusoPlatzi/ViewController.swift
+++ CusoPlatzi/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Referencias UI
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonAction(_ sender: Any) {
+        //1. Obtener los valores de los TextField
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        if email == "jorge@gmail.com", password == "123" {
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        } else {
+            print("Credenciales invalidas")
+        }
+        
+    }
+    
 }
 
