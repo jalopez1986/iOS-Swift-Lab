de1aaa55d509c8126e334233bebbd8439c75b54a
Jorge Lopez <jlobeto@gmail.com>
2021-03-27 23:53:55 -0500

    add cocoapods and popupdialog library

--- CusoPlatzi/ViewController.swift
+++ CusoPlatzi/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import PopupDialog
+
 
 class ViewController: UIViewController {
     //MARK: Referencias UI
@@ -42,9 +44,33 @@ class ViewController: UIViewController {
             performSegue(withIdentifier: "home_segue", sender: nil)
             
         } else {
-            print("Credenciales invalidas")
+            showErrorDialog()
         }
         
+    }
+    
+    private func showErrorDialog() {
+        // Prepare the popup assets
+        let title = "Error"
+        let message = "Credenciales inválidas"
+        let image = UIImage(named: "pexels-photo-103290")
+
+        // Create the dialog
+        let popup = PopupDialog(title: title, message: message, image: image)
+
+        // Create buttons
+        let buttonOne = CancelButton(title: "CANCEL") {
+            print("You canceled the car dialog.")
+        }
+
+
+        // Add buttons to dialog
+        // Alternatively, you can use popup.addButton(buttonOne)
+        // to add a single button
+        popup.addButtons([buttonOne])
+
+        // Present dialog
+        self.present(popup, animated: true, completion: nil)
     }
     
 }
